554898ab380d7bf6d597e3e911510ca54c6a808d
Dave Landry <landry.dave@gmail.com>
2015-04-14 12:29:11 -0400

    cleaner mocha output

--- test/mocha/Color.js
+++ test/mocha/Color.js
@@ -2,10 +2,12 @@ var assert = require("assert"),
     Color = require("../../src/Color/Color.js");
 
 describe("Color", function(){
-  describe("#constructor", function(){
-    it("should parse hex exactly as passed", function(){
+  describe("constructor", function(){
+    describe("hex parsing", function(){
       for (let hex of ["#cc0000", "#00ff00", "#000088"])
-        assert.equal(hex, new Color(hex).hex);
+        it(hex, function(){
+          assert.strictEqual(hex, new Color(hex).hex);
+      });
     });
   });
 });
