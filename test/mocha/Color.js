a6c44d0d31ee971a9a2717766129ad79af4ef082
Dave Landry <landry.dave@gmail.com>
2015-04-14 13:53:15 -0400

    Color test for missing values

--- test/mocha/Color.js
+++ test/mocha/Color.js
@@ -7,7 +7,13 @@ describe("Color", function(){
       for (let hex of ["#cc0000", "#00ff00", "#000088"])
         it(hex, function(){
           assert.strictEqual(hex, new Color(hex).hex);
-      });
+        });
+    });
+    describe("missing value parsing", function(){
+      for (let hex of [null, undefined])
+        it(hex, function(){
+          assert.strictEqual("#cccccc", new Color(hex).hex);
+        });
     });
   });
 });
