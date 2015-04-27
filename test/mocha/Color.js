67aaccfac1ed92e6a7dbca229240485370cef930
Dave Landry <landry.dave@gmail.com>
2015-04-26 21:34:10 -0300

    additional Color constructor tests

--- test/mocha/Color.js
+++ test/mocha/Color.js
@@ -2,18 +2,46 @@ var assert = require("assert"),
     Color = require("../../src/Color/Color.js");
 
 describe("Color", function(){
+
   describe("constructor", function(){
+
+    // Tests normal hex parsing (pass-through).
     describe("hex parsing", function(){
       for (let hex of ["#cc0000", "#00ff00", "#000088"])
         it(hex, function(){
           assert.strictEqual(hex, new Color(hex).hex);
         });
     });
+
+    // Tests for missing values.
     describe("missing value parsing", function(){
       for (let hex of [null, undefined])
         it(hex, function(){
           assert.strictEqual("#cccccc", new Color(hex).hex);
         });
     });
+
+    // Tests true/false booleans.
+    describe("boolean parsing", function(){
+      it("true", function(){
+        assert.strictEqual("#224f20", new Color(true).hex);
+      });
+      it("false", function(){
+        assert.strictEqual("#b22200", new Color(false).hex);
+      });
+    });
+
+    // Tests mapping strings and numbers to the default color scale.
+    describe("mapping to color scale", function(){
+      var scale = ["#b22200", "#eace3f", "#282f6b", "#b35c1e"],
+          values = ["Alpha", "Beta", 45, 85];
+      values.forEach(function(value, i){
+        it(value, function(){
+          assert.strictEqual(scale[i], new Color(value).hex);
+        });
+      });
+    });
+
   });
+
 });
